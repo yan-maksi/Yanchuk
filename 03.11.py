cce089f13394bf733b75b5beff1971f642cfdaf2
yan-maksi <102655652+yan-maksi@users.noreply.github.com>
2022-11-11 20:08:49 +0300

    Update 03.11.py

--- 03.11.py
+++ 03.11.py
@@ -6,7 +6,14 @@ def sniffing (interface):
 
 
 def process_packet(packet):
-    print(packet.show())
-
+    stream = packet.show()
+    print(stream)
+    return stream
 
 sniffing(None)
+
+def save_data_to_file(stream):
+    with open(stream, "w") as file:
+        file.write(stream)
+        
+save_data_to_file(process_packet())
